bc3a4e4a7f880fc7ff0abea98c06736a9af5b835
Vinícius Regatieri <viniciusvcr@hotmail.com>
2020-11-02 16:06:06 -0300

    run usado nos testes

--- run.sh
+++ run.sh
@@ -28,22 +28,24 @@ sizes=(2500 5000 10000 15000)
 
 seq_time=()
 par_time=()
+par_time_comms=()
 
 echo "Execução para ${n_processes} processos:"
 echo
 echo
 
 for size in ${sizes[@]}; do
-    ./${seq_executable} -n ${size}
+    echo "Size ${size}"
+    ./${seq_executable} -n ${size} > /dev/null
     for i in $(eval echo "{1..$n_repeticoes}"); do
         seq_time+=($(./${seq_executable} -n ${size} | grep "Time elapsed" | grep -Eo "?([0-9]*[.])?[0-9]+"))
-        par_time+=($(mpirun --host ${HOSTNAME}:${n_processes} ./${par_executable} -n ${size}| grep "Time elapsed" | grep -Eo "?([0-9]*[.])?[0-9]+"))
+        par_run=$(sudo perf stat -o output/perf_${size}_${n_processes}_${i} -B -e cache-references,cache-misses,cycles,instructions,branches,branch-misses,faults mpirun --host ${HOSTNAME}:${n_processes} ./${par_executable} -n ${size})
+        par_time+=($(echo -e ${par_run} | grep "Time elapsed" | grep -Eo "?([0-9]*[.])?[0-9]+" | head -n1))
+        par_time_comms+=($(echo -e ${par_run} | grep "Time with comms" | grep -Eo "?([0-9]*[.])?[0-9]+" | tail -n1))
     done
 
     python3 speedup.py $size $n_processes ${n_repeticoes} ${seq_time[@]} ${par_time[@]} > output/speedup${n_processes}_size_${size}
-
-    echo ${seq_time[@]} > output/seq_times_${n_processes}_${size}
-    echo ${par_time[@]} > output/par_times_${n_processes}_${size}
+    python3 speedup.py $size $n_processes ${n_repeticoes} ${seq_time[@]} ${par_time_comms[@]} > output/speedup_comm${n_processes}_size_${size}
 
     seq_time=()
     par_time=()
